7942ffead0226914ff3522ef69f9350e81dfeb3f
Furioza <t_sunny@mail.ru>
2018-07-13 07:44:52 +0300

    +new test

--- test/pal_test.exs
+++ test/pal_test.exs
@@ -14,6 +14,10 @@ defmodule PalTest do
       assert Pal.is_palyndrom?("А роза упала на лапу Азора")==true
    end
    
+   test "valid request_2" do
+      assert Pal.is_palyndrom?("А лис, он умён — крыса сыр к нему носила.")==true
+   end
+   
    test "invalid request" do
       assert Pal.is_palyndrom?("энциклопедия")==false
    end
