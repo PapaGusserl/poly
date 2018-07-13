a5875fbc8cdc38dfac6236ac992ca8ced1e0ef09
Furioza <t_sunny@mail.ru>
2018-07-13 22:03:49 +0300

    +describe и 2 доп.теста

--- test/pal_test.exs
+++ test/pal_test.exs
@@ -1,34 +1,41 @@
 defmodule PalTest do
   use ExUnit.Case, async: true
-  
-  ## Привет! У тебя в файлике много ненужных пробелов. В твоем редакторе кода наверняка есть функция для этого. trailing whitespace
-  # Посмотри в гугле про это)
-  # А ненужные пробелы - это зло. Баласт.
 
+  describe "Заполнение межбуквенного пространства" do
   test "empty string" do
       assert Pal.is_palyndrom?("")==true
    end
-   
-   test "valid request" do
+   end
+
+    describe "Положительные проверки" do
+    test "valid request" do
       assert Pal.is_palyndrom?("потоп")==true
    end
-   
+
    test "valid request_1" do
       assert Pal.is_palyndrom?("А роза упала на лапу Азора")==true
    end
 
-   ## Этот тест не должен проходить, так как ё и е - разные буквы
-   # Поэтому давай здесь сравнивать с false ИЛИ замени assert на refute
    test "valid request_2" do
-      refute Pal.is_palyndrom?("А лис, он умён — крыса сыр к нему носила.")==true
+      assert Pal.is_palyndrom?("20800802")==true
    end
-   
-   ## А этот тест пройдет
+
    test "valid request_2.1" do
       assert Pal.is_palyndrom?("А лис, он умен — крыса сыр к нему носила.")==true
    end
+   end
 
+   describe "Отрицательные проверки" do
    test "invalid request" do
       assert Pal.is_palyndrom?("энциклопедия")==false
+   end
+
+   test "invalid request_1" do
+      refute Pal.is_palyndrom?("А лис, он умён — крыса сыр к нему носила.")==true
+   end
+
+   test "invalid request_2" do
+    assert Pal.is_palyndrom?("А в Енисее —5 синева.")==false
+   end
    end
 end
